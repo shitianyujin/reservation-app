70a1328f406e7affdd3dc9cce080dd5f4658d4c5
shitianyujin <>
2020-03-31 22:27:49 +0900

    画像追加

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,29 +4,41 @@ export const products = [
     name: 'ユキさん',
     price: 6000,
     description: '新参者',
-    img: '../assets/img/yuki.jpg',
+    icon: '../assets/img/yuki.jpg',
+    coverImage: '../assets/img/yuki_cover.jpg',
     heading1: 'もっちりアザラシビッグクッション',
     heading2: '丸いって、平和',
-    heading3: '大阪の水族館・海遊館とYOU+MORE!のコラボレーション第三弾'
+    heading3: '大阪の水族館・海遊館とYOU+MORE!のコラボレーション第三弾',
+    heading1text: '高い',
+    heading2text: '丸い',
+    heading3text: '可愛い'
   },
   {
     id: 1,
     name: 'ウォンバットさん',
     price: 3500,
     description: 'ニート',
-    img: '../assets/img/wombat.jpg',
+    icon: '../assets/img/wombat.jpg',
+    coverImage: '../assets/img/wombat_cover.jpg',
     heading1: 'ぬいぐるみから生まれたキャラクター',
     heading2: 'おにぎり大好き',
-    heading3: '食べることと動かないことが大好き！ '
+    heading3: '食べることと動かないことが大好き！ ',
+    heading1text: 'デブ',
+    heading2text: 'ひきこもり',
+    heading3text: 'ロングスリーパー'
   },
   {
     id: 2,
-    name: 'あいつ',
+    name: 'ミ○キー',
     price: 1500,
     description: '扱われ方が雑なやつ',
-    img: '../assets/img/micky.jpg',
+    icon: '../assets/img/micky.jpg',
+    coverImage: '../assets/img/mickey_cover.jpg',
     heading1: '1928年（昭和3年）11月18日にスクリーンデビュー',
     heading2: '中国では「米老鼠」',
-    heading3: 'もともとは「モーティマーマウス」と名をつけるつもりだった。'
+    heading3: 'もともとは「モーティマーマウス」と名をつけるつもりだった。',
+    heading1text: '人気者',
+    heading2text: '世界一',
+    heading3text: '超有名'
   }
 ];
